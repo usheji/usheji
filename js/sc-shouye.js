130c9a1d76eaaf8102d9445c5baa86d456437588
sucong <183781873@qq.com>
2017-05-21 21:08:25 +0800

    sc

---
+++ js/sc-shouye.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/5/21.
+ */
+$(function () {
+    var jh=$('.jiaohu>div');
+    $(jh).on('click',function () {
+        $(jh).addClass('active');
+    })
+});
