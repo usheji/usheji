b138f5b4ce4363c4d88a3e4b6b5a793a67f39876
bloodkey <3028336816@qq.com>
2017-05-22 12:34:25 +0800

    '搜索页更名及部分效果'

--- js/common.js
+++ js/common.js
@@ -4,7 +4,7 @@
         recalc = function () {
             var clientWidth = docEl.clientWidth;
             if (!clientWidth) return;
-            if(clientWidth>=750){
+            if(clientWidth>=9999){
                 // 这里的640 取决于设计稿的宽度
                 docEl.style.fontSize = '100px';
             }else{
---
+++ js/zl-sousuo.js
@@ -0,0 +1,43 @@
+$(function(){
+	window.onload=function(){
+		
+// 上下滚	    
+		var myScroll;
+
+
+		myScroll = new IScroll('#wrapper', { mousewheel:true,scrollbars:true,
+		fadeScrollbars:true, shrinkScrollbars:'scale',click:true
+
+
+		});	
+	}
+// 滑动
+		var swiper1 = new Swiper('.swiper-container1', {
+        //  pagination: '.swiper-pagination',
+	        slidesPerView: 4,
+	        // slidesOffsetBefore : 15,
+	        // slidesOffsetAfter : -100,
+	        paginationClickable: true,
+	        simulateTouch : true,
+	        // spaceBetween: 30,
+	        freeMode: true
+	    });
+// 点赞取消赞
+	
+	$('.ly-cellect1').on('touchstart',function(){
+		
+		var v=$(this).children('p').text();
+		if($(this).attr('class')=='ly-cellect1 zl-z'){
+			$(this).children('p').html(v-1)
+			$(this).children('img').attr('src','../img/ly/ly-white.png')
+		}else{
+			$(this).children('p').html(v-0+1)
+			$(this).children('img').attr('src','../img/ly/ly-red.png')
+		}
+		$(this).toggleClass('zl-z');
+	})	    
+// 返回按钮
+	$('.ly-return').on('touchstart',function(){
+		window.history.back();
+	})
+})
